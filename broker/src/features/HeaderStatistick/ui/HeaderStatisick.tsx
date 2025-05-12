fd83662388ebc03d355dffeb2edb6d5aeb0b52a2
Monke72 <wera16307@gmail.com>
2025-05-12 14:23:32 +0400

    fix - исправлена работа выбора временного интервала , добавлен обработчик ошибок

--- broker/src/features/HeaderStatistick/ui/HeaderStatisick.tsx
+++ broker/src/features/HeaderStatistick/ui/HeaderStatisick.tsx
@@ -2,24 +2,57 @@ import HeaderInfo from "@shared/ui/HeaderInfo/HeaderInfo";
 import { DatePicker } from "antd";
 import dayjs from "dayjs";
 import cls from "./HeaderStatisick.module.scss";
+import type { DatePickerProps } from "antd";
+import { useEffect, useState } from "react";
+import { dateCompare } from "@shared/utils/dateCompare";
 
 const HeaderStatisick = () => {
+  const [dateStart, setDateStart] = useState<string | string[]>("");
+  const [dateFinish, setDateFinish] = useState<string | string[]>("");
+  const [dateError, setDateError] = useState<boolean>(false);
   const disabledDate = (current) => {
     // Запретить все даты после сегодняшнего дня
     return current && current > dayjs().endOf("day");
   };
+  const onChangeStart: DatePickerProps["onChange"] = (date, dateString) => {
+    setDateStart(dateString);
+  };
+  const onChangeFinish: DatePickerProps["onChange"] = (date, dateString) => {
+    setDateFinish(dateString);
+  };
+  useEffect(() => {
+    if (dateStart === "" || dateFinish === "") return;
+    if (typeof dateStart === "string" && typeof dateFinish === "string") {
+      const isLater = dateCompare({ date1: dateStart, date2: dateFinish });
+      setDateError(!isLater); // Пример: ошибка, если дата начала не раньше даты окончания
+    }
+  }, [dateFinish, dateStart]);
+  console.log(dateError);
+
   return (
     <HeaderInfo title="Статистика">
       <div className={cls["stat"]}>
         <div className={cls["stat__calendar"]}>
-          <DatePicker size="large" className={cls["stat__picker"]} />
+          <DatePicker
+            disabledDate={disabledDate}
+            size="large"
+            className={cls["stat__picker"]}
+            onChange={onChangeStart}
+          />
           <span> - </span>
           <DatePicker
+            onChange={onChangeFinish}
             disabledDate={disabledDate}
             className={cls["stat__picker"]}
             size="large"
           />
+          {dateError && (
+            <span className={cls["stat__picker-error"]}>
+              Введите корректный интервал
+            </span>
+          )}
         </div>
+
         <ul className={cls["stat__list"]}>
           <li className={cls["stat__item"]}>
             <h6 className={cls["stat__info"]}>Потенциальный доход</h6>
